019b843199cec910b6c55f6ef1da69fc657bbd55
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-07-03 14:33:00 +0100

    Ex 1 to 4 practice

---
+++ index.js
@@ -0,0 +1,31 @@
+let additionEquation = 12 + 20
+console.log(additionEquation)
+
+/* EXERCISE 2
+Create a variable named x containing the number 12. Then, UNCOMMENT the "return x" line.
+*/
+
+function ex2(){
+    let x = 12
+    return x
+}
+
+/* EXERCISE 3
+Create a variable named name containing the string John Doe Then, UNCOMMENT the "return name" line.
+*/
+
+function ex3(){
+    let name = "John Doe" 
+   return name
+}
+
+/* EXERCISE 4
+Execute a SUBTRACTION between the number 12 and the variable x, which stores the value 12.
+Then, UNCOMMENT the commented out lines.
+*/
+
+function ex4(){
+   let x = 12
+   let result = x - 12
+   return result
+}
