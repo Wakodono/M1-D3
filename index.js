e82ef09bba7b0722b3ceee380b55eab75a9314f6
Wakodono <53127265+Wakodono@users.noreply.github.com>
2021-07-24 18:28:12 +0100

    Ex 1 to 7

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/* EXERCISE 1
+Get the result of the SUM of 12 and 20
+*/
+
 let additionEquation = 12 + 20
 console.log(additionEquation)
 
@@ -5,30 +9,20 @@ console.log(additionEquation)
 Create a variable named x containing the number 12. Then, UNCOMMENT the "return x" line.
 */
 
-function ex2(){
-    let x = 12
-    return x
-}
+ let x = 12    
 
 /* EXERCISE 3
 Create a variable named name containing the string John Doe Then, UNCOMMENT the "return name" line.
 */
 
-function ex3(){
-    let name = "John Doe" 
-   return name
-}
+let name = "John Doe"
 
 /* EXERCISE 4
 Execute a SUBTRACTION between the number 12 and the variable x, which stores the value 12.
 Then, UNCOMMENT the commented out lines.
 */
 
-function ex4(){
-   let x = 12
-   let result = x - 12
-   return result
-}
+12 - x
 
 /* EXERCISE 5
 Create two variables: name1 and name2. name1 is equal to john, name2 is equal to John.
@@ -40,12 +34,41 @@ Then, UNCOMMENT the commented out lines.
 
 let name1 = "John"
 let name2 = "john"
+console.log(name1 === name2)
+console.log(name1.toLowerCase() === name2.toLowerCase())
 
-let areTheyEqual = name1 !== name2
-console.log(areTheyEqual)
+/* EXERCISE 6
+Write the code to print the literal value of a 
+number x: its value must be less than 10. (ex.: if x is 1 print "one", if 5 print "five" etc.) */
 
-name1.toLowerCase()
-name2.toLowerCase()
+x = 9
+switch(x){
+    case 1: console.log("one"); break;
+    case 2: console.log("two"); break;
+    case 3: console.log("three"); break;
+    case 4: console.log("four"); break;
+    case 5: console.log("five"); break;
+    case 6: console.log("six"); break;
+    case 7: console.log("seven"); break;
+    case 8: console.log("eight"); break;
+    case 9: console.log("nine"); break;
+    default: console.log("The number must be between 1 and 9")
+}
 
-let areTheyEqualNow = name1 == name2
-console.log("are they equal now" + ":" + areTheyEqualNow)
+/* EXERCISE 7
+Return the largest between two numbers a and b using a ternary operator.*/
+
+let a = 15
+let b = 99
+let isaBigger = a >= b
+let isbBigger = b >= a
+
+/* Expanded alternative
+if (isaBigger) {
+    console.log(a)
+} else {
+    console.log(b)
+} */
+
+// More condensed alternative: this line is exactly the same as the commented out IF-ELSE block above
+largestNumber = isaBigger ? console.log(a) : console.log(b)
